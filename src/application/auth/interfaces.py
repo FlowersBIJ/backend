0830554069f33ad5af6f8e0b8356e58bd54df737
LuosRemmus <17summersoul17@gmail.com>
2023-12-01 21:51:41 +0300

    ...

--- src/application/auth/interfaces.py
+++ src/application/auth/interfaces.py
@@ -1,98 +1,71 @@
-import abc
-
+from abc import ABC, abstractmethod
 from casdoor import AsyncCasdoorSDK, User
 
 
-class IAuth(abc.ABC):
-    def __init__(
-        self,
-        endpoint,
-        client_id,
-        client_secret,
-        certificate,
-        org_name,
-        application_name,
-    ) -> None:
-        self.sdk = AsyncCasdoorSDK(
-            endpoint=endpoint,
-            client_id=client_id,
-            client_secret=client_secret,
-            certificate=certificate,
-            org_name=org_name,
-            application_name=application_name,
-        )
+class BaseAuth(ABC):
+    def __init__(self,
+                 endpoint: str,
+                 client_id: str,
+                 client_secret: str,
+                 certificate: str,
+                 org_name: str,
+                 application_name: str,
+                 front_endpoint: str = None) -> None:
+        params = {
+            "endpoint": endpoint,
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "certificate": certificate,
+            "org_name": org_name,
+            "application_name": application_name,
+        }
+        if front_endpoint:
+            params["front_endpoint"] = front_endpoint
+        self.sdk = AsyncCasdoorSDK(**params)
 
-    @abc.abstractmethod
-    async def get_oauth_token(
-        self,
-        code: str | None = None,
-        username: str | None = None,
-        password: str | None = None,
-    ):
-        pass
 
-    @abc.abstractmethod
-    async def parse_jwt_token(self, token: str):
-        pass
+class IAuth(BaseAuth):
+    @abstractmethod
+    async def get_parsed_jwt_token(self, code):
+        raise NotImplementedError
 
-    @abc.abstractmethod
-    async def add_user(self, user: User):
-        pass
+    @abstractmethod
+    async def get_auth_link(self, redirect_uri: str, response_type: str = "code", scope: str = "read"):
+        raise NotImplementedError
 
-    @abc.abstractmethod
-    async def delete_user(self, user: User):
-        pass
-
-    @abc.abstractmethod
-    async def update_user(self, user: User):
-        pass
-
-    @abc.abstractmethod
-    async def read_user(self, user_id: str):
-        pass
-
-    @abc.abstractmethod
-    async def read_users(self):
-        pass
-
-    @abc.abstractmethod
-    async def batch_enforce(
-        self, permission_model_name: str, permission_rules: list[list[str]]
-    ):
-        pass
-
-    @abc.abstractmethod
-    async def enforce(
-        self,
-        permission_model_name: str,
-        sub: str,
-        obj: str,
-        act: str,
-        v3: str | None = None,
-        v4: str | None = None,
-        v5: str | None = None,
-    ):
-        pass
-
-    @abc.abstractmethod
-    async def get_auth_link(
-        self, redirect_uri: str, response_type: str = "code", scope: str = "read"
-    ):
-        pass
-
-    @abc.abstractmethod
-    async def oauth_token_request(
-        self,
-        code: str | None = None,
-        username: str | None = None,
-        password: str | None = None,
-    ) -> dict:
-        pass
-
-    @abc.abstractmethod
+    @abstractmethod
     async def refresh_oauth_token(self, refresh_token: str, scope: str = "") -> str:
-        pass
+        raise NotImplementedError
 
-    @abc.abstractmethod
-    async def refresh_token_request(self, refresh_token: str, scope: str = "") -> dict:
-        pass
+
+class UserInterface(BaseAuth):
+    @abstractmethod
+    async def batch_enforce(
+            self, permission_model_name: str, permission_rules: list[list[str]]
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def enforce(
+            self,
+            permission_model_name: str,
+            sub: str,
+            obj: str,
+            act: str,
+            v3: str | None = None,
+            v4: str | None = None,
+            v5: str | None = None,
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def add_user(self, user: User):
+        return self.sdk.add_user(user)
+
+    @abstractmethod
+    async def update_user(self, user: User):
+        return self.sdk.update_user(user)
+
+    @abstractmethod
+    async def delete_user(self, user: User):
+        return self.sdk.delete_user(user)
